f3a14690f7bce9d0943c7719c88a70aac47acc75
IngDavidJimenez <davidjimenez.ing@gmail.com>
2023-03-25 03:36:36 +0000

    feat: Style CCS of errors

--- balance/models.py
+++ balance/models.py
@@ -1,6 +1,7 @@
 
 import csv
 from . import RUTA_FICHERO
+from datetime import date
 """
 Un movimiento debe tener:
 1. Fecha
@@ -10,10 +11,26 @@ Un movimiento debe tener:
 """
 class Movimiento:
     def __init__(self,fecha,concepto,tipo,cantidad):
-        self.fecha=fecha
+        self.errores=[]
+
+        try:
+            self.fecha=date.fromisoformat(fecha)
+        except ValueError:
+            self.fecha = None
+            self.errores.append("El formato de la fecha no es valido")
+        
         self.concepto=concepto
         self.tipo = tipo
         self.cantidad = cantidad
+    @property 
+    def has_errors(self):
+        return len(self.errores)> 0
+    
+    def __str__(self):
+        return f'{self.fecha}\t{self.concepto}\t{self.tipo}\t{self.cantidad}\t'
+    def __repr__(self):
+        return self.__str__()
+    
 
 class ListaMovimientos:
     """
@@ -28,9 +45,15 @@ class ListaMovimientos:
         #fichero.close()
         with open(RUTA_FICHERO,'r') as fichero:
             reader= csv.DictReader(fichero)
+            
             for fila in reader:
-                #print(fila['fecha'],fila['concepto'],fila['ingreso_gasto'],fila['cantidad'])
-                self.lista_movimientos.append(fila)
+                #print(fila['fecha'],fila['concepto'],
+                # fila['ingreso_gasto'],fila['cantidad'])
+                mov=Movimiento(fila['fecha'],
+                               fila['concepto'],
+                               fila['ingreso_gasto'],
+                               fila['cantidad'])
+                self.lista_movimientos.append(mov)
 
     
 
